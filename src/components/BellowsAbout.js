2d1cc35259f073f3edceb4161557102d83ce2af9
nickrosales <116924957+nickrosales@users.noreply.github.com>
2023-08-24 16:56:37 -0700

    added bootstrap to index.html

--- src/components/BellowsAbout.js
+++ src/components/BellowsAbout.js
@@ -3,10 +3,9 @@ import React from 'react'
 function Footer() {
 
     return (
-        <div className="card text-center">
-            <div className="card-body">
-                <h5 className="card-title">Marcus Paccapaniccia</h5>
-                <p className="card-text">&copy; 2023</p>
+        <div>
+            <div>
+                
             </div>
 
         </div>

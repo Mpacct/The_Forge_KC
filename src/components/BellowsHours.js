6e8bef05548428f064d7eb567ef9b400e082ca10
unknown <marcuspacc@gmail.com>
2023-08-24 16:48:16 -0700

    added template

---
+++ src/components/BellowsHours.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+
+    return (
+        <div className="card text-center">
+            <div className="card-body">
+                <h5 className="card-title">Marcus Paccapaniccia</h5>
+                <p className="card-text">&copy; 2023</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Footer;

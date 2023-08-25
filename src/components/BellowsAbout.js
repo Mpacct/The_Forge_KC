7cbd743e4ca564e888cbfb8761e7c50ae404c524
nickrosales <116924957+nickrosales@users.noreply.github.com>
2023-08-24 17:20:49 -0700

    added about componet

--- src/components/BellowsAbout.js
+++ src/components/BellowsAbout.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Footer() {
+function BellowsAbout() {
 
     return (
-        <div>
-            <div>
-                
+        <div class="card text-center" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">About The Bellows</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-
         </div>
     );
 }
 
-export default Footer;
+export default BellowsAbout;

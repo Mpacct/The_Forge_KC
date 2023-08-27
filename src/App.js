416d6f47e74ef2e5c18ba38ed5146da962451f91
unknown <marcuspacc@gmail.com>
2023-08-27 14:15:28 -0700

    updated navbar, header, and layout for site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BellowsFooter from './components/BellowsFooter';
+import BellowsHeader from './components/BellowsHeader';
 
 import Bellows from './pages/Bellows';
 
 function App() {
   return (
     <Router>
+      
       <div>
+
         <Routes>
           <Route
             path="/bellows"
             element={<Bellows />}
           />
-          <Route
+          {/* <Route
             path="*"
             element={<NoMatch />}
-          />
+          /> */}
         </Routes>
+
       </div>
     </Router>
   );
--- src/components/BellowsNavbar.js
+++ src/components/BellowsNavbar.js
@@ -1,16 +1,52 @@
 import React from 'react'
+import bellowsLogo from '../assets/images/bellowsIcon.PNG'
+import { Link } from 'react-router-dom'
+import '../assets/styles/bellowsHeader.css';
 
-function Footer() {
+function BellowsNavbar() {
 
     return (
-        <div className="card text-center">
-            <div className="card-body">
-                <h5 className="card-title">Marcus Paccapaniccia</h5>
-                <p className="card-text">&copy; 2023</p>
+        // <!-- Navbar -->
+        <nav className="bellowsHeader navbar navbar-expand-lg fixed-top bg-light navbar-light">
+            <div className="container">
+                <img
+                    id="MDB-logo"
+                    src={bellowsLogo}
+                    alt="The Bellows Logo"
+                    draggable="false"
+                    height="30"
+                />
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto align-items-center">
+                        <li className="nav-item">
+                            <Link to="/" className="text-white nav-link mx-2"> Post </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="text-white nav-link mx-2" href="#!"><i className="fas fa-bell pe-2"></i>Alerts</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="text-white nav-link mx-2" href="#!"><i className="fas fa-heart pe-2"></i>Trips</a>
+                        </li>
+                        <li className="nav-item ms-3">
+                            <a className="text-white btn btn-black btn-rounded" href="#!">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
-        </div>
+        </nav>
+        // <!-- Navbar -->
     );
 }
 
-export default Footer;
+export default BellowsNavbar;

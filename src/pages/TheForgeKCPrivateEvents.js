ac0c1fc8c0d7623a85cc7b3135394032c34c556e
unknown <marcuspacc@gmail.com>
2024-01-26 16:55:37 -0700

    created contact and private events pages

---
+++ src/pages/TheForgeKCPrivateEvents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TheForgeAbout from "../components/TheForgeAbout";
+
+function TheForgKCPrivateEvents() {
+
+    return (
+        <div >
+            <body className='bg-dark'>
+
+                <TheForgeAbout />
+            </body>
+            
+        </div>
+    );
+}
+
+export default TheForgKCPrivateEvents;
